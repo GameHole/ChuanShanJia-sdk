df2cfdcee86e8ac9962668b92e847a25e16cbe2f
GameHole <741419087@qq.com>
2021-03-15 17:10:07 +0800

    修改BUG

--- Plugins/Android/AdInit.java
+++ Plugins/Android/AdInit.java
@@ -1,6 +1,7 @@
 package com.unity.ttsdk;
 
 import android.app.Activity;
+import android.os.HandlerThread;
 import android.util.Log;
 
 import com.bytedance.sdk.openadsdk.TTAdConfig;
@@ -10,22 +11,30 @@ import com.bytedance.sdk.openadsdk.TTCustomController;
 import com.bytedance.sdk.openadsdk.TTLocation;
 
 public class AdInit{
-
-    public  static  void  Init(String mark,Activity activity,String appid,String appname,boolean debug,boolean enable){
-        TTAdConfig config = new TTAdConfig.Builder()
-                .appId(appid)
-                .useTextureView(false) //使用TextureView控件播放视频,默认为SurfaceView,当有SurfaceView冲突的场景，可以使用TextureView
-                .appName(appname)
-                .titleBarTheme(TTAdConstant.TITLE_BAR_THEME_DARK)
-                .allowShowNotify(true) //是否允许sdk展示通知栏提示
-                .allowShowPageWhenScreenLock(true) //是否在锁屏场景支持展示广告落地页
-                .debug(debug) //测试阶段打开，可以通过日志排查问题，上线时去除该调用
-                .directDownloadNetworkType(TTAdConstant.NETWORK_STATE_WIFI, TTAdConstant.NETWORK_STATE_3G,TTAdConstant.NETWORK_STATE_4G,TTAdConstant.NETWORK_STATE_MOBILE) //允许直接下载的网络状态集合
-                .supportMultiProcess(true) //是否支持多进程，true支持
-                .customController(getController(enable))//控制隐私数据
-                .build();
-        TTAdSdk.init(activity, config);
-        Log.v("Unity","aaaaaaa");
+    public  static  void  Init(String mark, final Activity activity, final String appid, final String appname, final boolean debug, final boolean enable,final  IInitOver ac){
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                TTAdConfig config = new TTAdConfig.Builder()
+                        .appId(appid)
+                        .useTextureView(false) //使用TextureView控件播放视频,默认为SurfaceView,当有SurfaceView冲突的场景，可以使用TextureView
+                        .appName(appname)
+                        .titleBarTheme(TTAdConstant.TITLE_BAR_THEME_DARK)
+                        .allowShowNotify(true) //是否允许sdk展示通知栏提示
+                        .allowShowPageWhenScreenLock(true) //是否在锁屏场景支持展示广告落地页
+                        .debug(debug) //测试阶段打开，可以通过日志排查问题，上线时去除该调用
+                        .directDownloadNetworkType(TTAdConstant.NETWORK_STATE_WIFI, TTAdConstant.NETWORK_STATE_3G,TTAdConstant.NETWORK_STATE_4G,TTAdConstant.NETWORK_STATE_MOBILE) //允许直接下载的网络状态集合
+                        .supportMultiProcess(true) //是否支持多进程，true支持
+                        .customController(getController(enable))//控制隐私数据
+                        .build();
+                TTAdSdk.init(activity, config);
+                Log.v("Unity","aaaaaaa");
+                if(ac!= null) {
+                    ac.OnOver();
+                }
+            }
+        });
+        thread.start();
     }
     private static TTCustomController getController(boolean enable) {
         MyTTCustomController customController = new MyTTCustomController();
